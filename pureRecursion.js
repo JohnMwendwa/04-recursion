8ab0d26750e77b986eec9ee08000ddffce56afdf
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-08-22 10:40:43 +0300

    create a pure recursion helper function

---
+++ pureRecursion.js
@@ -0,0 +1,13 @@
+function collectOddValues(arr) {
+  let newArr = [];
+  if (arr.length === 0) {
+    return newArr;
+  }
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
+
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]));
